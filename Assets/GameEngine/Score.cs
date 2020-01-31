4b8f36570369b18afc5831aa1eb8b2b55125bf92
Povilas <povilasleka@gmail.com>
2020-01-31 23:58:34 +0200

    Score script

---
+++ Assets/GameEngine/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
---
+++ Assets/GameEngine/SomeOtherScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeOtherScript : MonoBehaviour
+{
+
+    public Score score;
+    public GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.Find("Text");
+        score = text.GetComponent<Score>();
+
+        score.AddScore(10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.AddScore(10);
+    }
+}
