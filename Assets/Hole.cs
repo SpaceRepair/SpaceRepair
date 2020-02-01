53902d224ab999c3da16e98e3e6b4beca43d65a3
Povilas Galcius <povilas.galcius@devbridge.com>
2020-02-01 20:26:13 +0200

    Attach item to hole

--- Assets/Hole.cs
+++ Assets/Hole.cs
@@ -33,9 +33,15 @@ public class Hole : MonoBehaviour
         }
     }
 
-    public void FillMyself()
+    public void FillMyself(GameObject scrap)
     {
+        scrap.transform.parent = transform;
+        //scrap.transform.position = new Vector2(0f, 0f);
         var renderer = GetComponent<SpriteRenderer>();
-        renderer.material.SetColor("_Color", new Color(0f, 0f, 0f, 0.5f));
+        // NOT WORKING
+        // scrap.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        // scrap.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        //scrap.transform.ve
+        renderer.material.SetColor("_Color", new Color(1f, 1f, 1f, 0.5f));
     }
 }
